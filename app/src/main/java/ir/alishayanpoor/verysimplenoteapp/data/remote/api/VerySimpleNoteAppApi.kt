82b7e61f9be198371c4acb414e0843a1af94a4a2
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-01 14:02:48 +0430

    Create remote api interface

---
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/data/remote/api/VerySimpleNoteAppApi.kt
@@ -0,0 +1,21 @@
+package ir.alishayanpoor.verysimplenoteapp.data.remote.api
+
+import com.google.gson.JsonObject
+import ir.alishayanpoor.verysimplenoteapp.data.remote.dto.NewNoteDto
+import ir.alishayanpoor.verysimplenoteapp.data.remote.dto.NoteListDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+const val BASE_URL = "https://api.alishayanpoor.ir/Very-Simple-Note-App/api/v1/"
+
+interface VerySimpleNoteAppApi {
+    @GET("notes")
+    suspend fun getAllNotes(): Response<NoteListDto>
+
+    @POST("note")
+    suspend fun newNote(
+        @Body newNoteDto: NewNoteDto
+    ): Response<JsonObject>
+}
