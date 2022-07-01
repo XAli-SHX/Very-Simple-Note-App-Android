5a60e47a4b21d0df4272bdf3b2f9aaa07d6326a2
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-01 18:30:01 +0430

    Add a tag state

--- app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/new_note/NewNoteState.kt
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/new_note/NewNoteState.kt
@@ -3,6 +3,7 @@ package ir.alishayanpoor.verysimplenoteapp.ui.view.new_note
 data class NewNoteState(
     val title: String = "",
     val body: String = "",
+    val tag: String = "",
     val tags: List<String> = emptyList(),
     val isSendingNote: Boolean = false,
     val error: String? = null
