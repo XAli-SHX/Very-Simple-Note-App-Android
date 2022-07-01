0af4577a966070febc50ef2ee1e9c876b1c5960a
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-01 17:10:09 +0430

    Add New Note states

---
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/new_note/NewNoteState.kt
@@ -0,0 +1,9 @@
+package ir.alishayanpoor.verysimplenoteapp.ui.view.new_note
+
+data class NewNoteState(
+    val title: String = "",
+    val body: String = "",
+    val tags: List<String> = emptyList(),
+    val isSendingNote: Boolean = false,
+    val error: String? = null
+)
