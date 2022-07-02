0f72c0840c27adcec18106b466d07e09c2bb3f9d
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-02 14:21:16 +0430

    Create use case module to inject with dagger-hilt

---
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/di/UseCaseModule.kt
@@ -0,0 +1,20 @@
+package ir.alishayanpoor.verysimplenoteapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ir.alishayanpoor.verysimplenoteapp.data.remote.api.VerySimpleNoteAppApi
+import ir.alishayanpoor.verysimplenoteapp.domain.use_case.CreateNewNoteUseCase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class UseCaseModule {
+
+    @Singleton
+    @Provides
+    fun createNewNoteUseCase(verySimpleNoteAppApi: VerySimpleNoteAppApi): CreateNewNoteUseCase {
+        return CreateNewNoteUseCase(verySimpleNoteAppApi)
+    }
+}
