1648e49438de0bad32dc6014b7d1944e3a2118f4
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-02 14:19:50 +0430

    Put item texts in box with more beautiful ui

--- app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/list_note/NoteListActivity.kt
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/list_note/NoteListActivity.kt
@@ -1,12 +1,17 @@
 package ir.alishayanpoor.verysimplenoteapp.ui.view.list_note
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
@@ -16,6 +21,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.swiperefresh.SwipeRefresh
@@ -30,6 +36,13 @@ import kotlinx.coroutines.flow.receiveAsFlow
 @AndroidEntryPoint
 class NoteListActivity : ComponentActivity() {
     private val viewModel: NoteListViewModel by viewModels()
+    private val newNoteResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                viewModel.loadNotes()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +61,7 @@ class NoteListActivity : ComponentActivity() {
             when (event) {
                 NoteListEvent.CreateNewNote -> {
                     Intent(this, NewNoteActivity::class.java).also {
-                        startActivity(it)
+                        newNoteResultLauncher.launch(it)
                     }
                 }
             }.exhaustive
@@ -84,12 +97,24 @@ class NoteListActivity : ComponentActivity() {
                         LazyColumn(
                             modifier = Modifier.fillMaxSize()
                         ) {
-                            viewModel.state.noteList.forEach { note ->
+                            viewModel.state.noteList.forEachIndexed { index, note ->
                                 item {
-                                    Text(
-                                        text = note.title,
-                                        modifier = Modifier.fillMaxWidth()
-                                    )
+                                    Box(
+                                        modifier = Modifier
+                                            .border(
+                                                width = 0.8.dp,
+                                                color = Color.Gray.copy(alpha = 0.5f),
+                                                shape = RoundedCornerShape(12.dp)
+                                            )
+                                    ) {
+                                        Text(
+                                            text = note.title,
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(12.dp)
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.height(12.dp))
                                 }
                             }
                         }
