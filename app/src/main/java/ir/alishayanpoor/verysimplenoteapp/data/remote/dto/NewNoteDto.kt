7ff73afe9ab9185e87cb522330845b1734cf3fde
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-02 14:18:09 +0430

    Make static function to convert NewNoteDto into Note model

--- app/src/main/java/ir/alishayanpoor/verysimplenoteapp/data/remote/dto/NewNoteDto.kt
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/data/remote/dto/NewNoteDto.kt
@@ -2,6 +2,7 @@ package ir.alishayanpoor.verysimplenoteapp.data.remote.dto
 
 
 import com.google.gson.annotations.SerializedName
+import ir.alishayanpoor.verysimplenoteapp.domain.model.Note
 
 data class NewNoteDto(
     @SerializedName("title")
@@ -10,4 +11,14 @@ data class NewNoteDto(
     val body: String,
     @SerializedName("tags")
     val tags: List<String>
-)
+) {
+    companion object {
+        fun fromNote(note: Note): NewNoteDto {
+            return NewNoteDto(
+                note.title,
+                note.body,
+                note.tags,
+            )
+        }
+    }
+}
