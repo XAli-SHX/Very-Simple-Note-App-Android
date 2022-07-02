4ef5f44649dab5cf1a27f7ded6b7669e17fe0244
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-02 14:16:39 +0430

    Change Submit and remove parameters

--- app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/new_note/NewNoteAction.kt
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/new_note/NewNoteAction.kt
@@ -4,7 +4,7 @@ sealed class NewNoteAction {
     data class Title(val title: String) : NewNoteAction()
     data class Body(val body: String) : NewNoteAction()
     data class Tag(val tag: String) : NewNoteAction()
-    data class SubmitTag(val tag: String) : NewNoteAction()
-    data class RemoveTag(val tag: String) : NewNoteAction()
+    object SubmitTag : NewNoteAction()
+    data class RemoveTag(val index: Int) : NewNoteAction()
     object Create : NewNoteAction()
 }
