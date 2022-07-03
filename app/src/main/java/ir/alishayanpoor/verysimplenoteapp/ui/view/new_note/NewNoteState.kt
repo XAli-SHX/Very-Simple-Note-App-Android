2668c0c68739e16609c1371a82e2540dd7553093
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-07-04 00:48:48 +0430

    Change view mode from bool to enum

--- app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/new_note/NewNoteState.kt
+++ app/src/main/java/ir/alishayanpoor/verysimplenoteapp/ui/view/new_note/NewNoteState.kt
@@ -1,12 +1,17 @@
 package ir.alishayanpoor.verysimplenoteapp.ui.view.new_note
 
 data class NewNoteState(
+    val id: Int? = null,
     val title: String = "",
-    val viewMode: Boolean = false,
+    val viewMode: ViewMode = ViewMode.New,
     val body: String = "",
     val tag: String = "",
     val tags: List<String> = emptyList(),
     val isSendingNote: Boolean = false,
     val titleError: String? = null,
     val bodyError: String? = null,
-)
+) {
+    enum class ViewMode {
+        View, Edit, New
+    }
+}
